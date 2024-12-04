98ba3471b76e4e7c7556acc8276dfa44dc883017
rbs-dev <111300320+dev-royalbluesoftware@users.noreply.github.com>
2024-12-03 22:24:14 -0700

    Complete thru 1.12

---
+++ YouAreAwesome/YouAreAwesome/ContentView.swift
@@ -0,0 +1,66 @@
+//
+//
+// YouAreAwesome
+// ContentView.swift
+//
+// Created by rbs-dev
+// Copyright © Royal Blue Software
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    // MARK: - State Properties
+    
+    @State private var messageString = ""
+    @State private var imageName = ""
+    @State private var imageNumber = 0
+    
+    // MARK: - View Body
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 15))
+                .shadow(radius: 15)
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.75)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.indigo)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+            
+            Spacer()
+            
+            Button("Show Message") {
+                // This is the action performed when the button is pressed
+                let messageAwesome = "You Are Awesome!"
+                let messageGreat = "You Are Great!"
+                
+                messageString = messageString.isEmpty ? messageAwesome : messageString == messageAwesome ? messageGreat : messageAwesome
+                
+                imageName = "image\(imageNumber)"
+                imageNumber += 1
+                if imageNumber > 9 {
+                    imageNumber = 0
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.green)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreAwesome/YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,20 @@
+//
+//
+// YouAreAwesome
+// YouAreAwesomeApp.swift
+//
+// Created by rbs-dev
+// Copyright © Royal Blue Software
+// 
+
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

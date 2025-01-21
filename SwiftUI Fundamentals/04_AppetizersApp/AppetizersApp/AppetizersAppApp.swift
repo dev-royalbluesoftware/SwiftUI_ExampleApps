ede3767962418988f735537d25812e0cf1885897
rbs-dev <111300320+dev-royalbluesoftware@users.noreply.github.com>
2025-01-20 21:45:05 -0700

    AppetizersApp initial setup

---
+++ SwiftUI Fundamentals/04_AppetizersApp/AppetizersApp/AppetizersAppApp.swift
@@ -0,0 +1,20 @@
+//
+//
+// AppetizersApp
+// AppetizersAppApp.swift
+//
+// Created by rbs-dev
+// Copyright © Royal Blue Software
+// 
+
+
+import SwiftUI
+
+@main
+struct AppetizersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

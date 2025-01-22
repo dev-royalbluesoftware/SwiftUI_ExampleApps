6d2fd7b14757128d738560acd5ddff0c8133cab6
rbs-dev <111300320+dev-royalbluesoftware@users.noreply.github.com>
2025-01-21 20:14:35 -0700

    Swift Playground

---
+++ SwiftfulThinkingBootcamp.playground/Pages/01-SwiftBasics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+print(greeting)
+
+// This is a single-line comment
+
+/*
+ This is a multi-line comment
+ this can
+ span multiple lines
+ */
+
+// MARK: - NAMING CONVENTIONS
+
+let firstGreeting = "Hello, World!" // this is camelCase (first word lowercase, following words Uppercase)
+
+// Camel Case is the standard for the Swift Language
+
+//: [Next](@next)
---
+++ SwiftfulThinkingBootcamp.playground/Pages/03-VariablesConstants.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+import Foundation
+
+
+// MARK: - VARIABLES and CONSTANTS
+
+// Constants - value that will not change - immutable
+
+let someConstant = 42
+//someConstant = 200  - this creates an error because it cannot be changed
+
+// Variables - value that can change - mutable
+
+var someVariable = 42 // Type Inference - Swift will infer the type - (Int)
+someVariable = 200
+print(someVariable)
+
+//: [Next](@next)

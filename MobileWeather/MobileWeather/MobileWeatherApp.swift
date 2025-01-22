e87ce912bfab9a3b1b19f741a756d1103d4a8bc1
rbs-dev <111300320+dev-royalbluesoftware@users.noreply.github.com>
2025-01-21 23:46:52 -0700

    Mobile Weather App initial setup

---
+++ MobileWeather/MobileWeather/MobileWeatherApp.swift
@@ -0,0 +1,20 @@
+//
+//
+// MobileWeather
+// MobileWeatherApp.swift
+//
+// Created by rbs-dev
+// Copyright © Royal Blue Software
+// 
+
+
+import SwiftUI
+
+@main
+struct MobileWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

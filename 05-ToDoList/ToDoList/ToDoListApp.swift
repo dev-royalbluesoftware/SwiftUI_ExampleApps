7e0afd1844e7cad3000970680225ef5ce2c343b2
rbs-dev <111300320+dev-royalbluesoftware@users.noreply.github.com>
2025-01-22 13:47:36 -0700

    ToDoList app initial setup

---
+++ 05-ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,20 @@
+//
+//
+// ToDoList
+// ToDoListApp.swift
+//
+// Created by rbs-dev
+// Copyright © Royal Blue Software
+// 
+
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

1b2feba21b337c2562d66e65f6701691ac3e654b
rbs-dev <111300320+dev-royalbluesoftware@users.noreply.github.com>
2025-02-01 12:26:49 -0700

    Step-Tracker app initial commit

---
+++ Step-Tracker/Step-Tracker/Step_TrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//
+// Step-Tracker
+// Step_TrackerApp.swift
+//
+// Created by rbs-dev
+// Copyright © Royal Blue Software
+// 
+
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
